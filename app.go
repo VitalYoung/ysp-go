4aa77ec60a1270a2dea03465f16def8a0f375d65
Yang Zhu <yang.zhu@hongjingdrive.com>
2020-07-20 14:28:32 +0800

    go

---
+++ app.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "net/http"
+  "github.com/go-chi/chi"
+  "github.com/go-chi/chi/middleware"
+)
+
+func main() {
+  r := chi.NewRouter()
+  r.Use(middleware.Logger)
+  r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Welcome go chi\n"))
+  })
+  http.ListenAndServe(":3000", r)
+}
